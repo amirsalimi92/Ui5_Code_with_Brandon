4ce0e70c400dc9629d00fbc75a2107fe07f01d7b
Amir Salimi <amir.salimi1810@gmail.com>
2025-03-18 21:18:53 +0100

    Before Component

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,45 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
-  'use strict'
-  return Controller.extend('myapp.App', {
-    onShowHello: function () {
-      alert('Hello there!')
-    }
-  })
-})
+sap.ui.define(
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel'
+  ],
+  function (Controller, MsgToast, JsonM, ResModel) {
+    'use strict'
+    return Controller.extend('myapp.App', {
+      onInit: function () {
+        // Set the data model on the view
+        var oData = {
+          recipient: {
+            name: 'Ui5'
+          }
+        }
+
+        var oModel = new JsonM(oData)
+        this.getView().setModel(oModel)
+
+        // set i18n model
+        var i18nModel = new ResModel({
+          bundleName: 'myapp.i18n.i18n',
+          supportedLocales: [''],
+          fallbackLocale: ''
+        })
+        this.getView().setModel(i18nModel, 'i18n')
+      },
+      onShowHello: function () {
+        var oModel = this.getView().getModel()
+        var sName = oModel.getProperty('/recipient/name')
+
+        // Read the i18n model
+        var oBundle = this.getView().getModel('i18n').getResourceBundle()
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty('/recipient/name')
+        var sMsg = oBundle.getText('helloMsg', [sRecipient])
+
+        MsgToast.show(sMsg)
+      }
+    })
+  }
+)
