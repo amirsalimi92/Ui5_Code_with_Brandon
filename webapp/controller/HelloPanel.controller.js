e44f9815b1b6c3fea4976a817d755ee699c13105
Amir Salimi <amir.salimi1810@gmail.com>
2025-03-19 01:17:38 +0100

    Implemet the dialog inside an additional view

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast'],
-  function (Controller, MsgToast) {
+  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast', 'sap/ui/core/Fragment'],
+  function (Controller, MsgToast, Fragment) {
     'use strict'
     return Controller.extend('myapp.controller.HelloPanel', {
       onShowHello: function () {
@@ -16,6 +16,30 @@ sap.ui.define(
         var sMsg = oBundle.getText('helloMsg', [sRecipient])
 
         MsgToast.show(sMsg)
+      },
+
+      onOpenDialog: function () {
+        var oView = this.getView()
+
+        // Create the dialog lazily
+        if (!this.byId('helloDialog')) {
+          // Load asynchronous XML Fragment
+          Fragment.load({
+            id: oView.getId(),
+            name: 'myapp.view.HelloDialog',
+            controller: this
+          }).then(function (oDialog) {
+            // Connect dialog to the root view of component (model, lifecycle)
+            oView.addDependent(oDialog)
+            oDialog.open()
+          })
+        } else {
+          this.byId('helloDialog').open()
+        }
+      },
+
+      onCloseDialog: function () {
+        this.byId('helloDialog').close()
       }
     })
   }
